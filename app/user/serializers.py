93efd3b2eca85de43a821d25b534db7f2da5e6b4
Gankin <sergey.gankin.ext@suez.com>
2020-11-09 15:35:09 -0500

    Re-factored tests and ingredients.

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -15,16 +15,15 @@ class UserSerializer(serializers.ModelSerializer):
     return get_user_model().objects.create_user(**validated_data)
 
   def update(self, instance, validated_data):
-    """Update a user, setting the password correctly and returning it"""
-    password = vaidated_data.pop('password', None)
-    user = super().update(instance, vaidated_data)
+    """Update a user, setting the password correctly and return it"""
+    password = validated_data.pop('password', None)
+    user = super().update(instance, validated_data)
 
     if password:
       user.set_password(password)
       user.save()
 
     return user
-
 
 class AuthTokenSerializer(serializers.Serializer):
   """Serialized for the user authentication object"""
